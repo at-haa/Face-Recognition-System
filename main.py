fc395cb9e4b2f85518428292dd9e38ae8af34c91
fateme1929 <atoosa.havaseli@gmail.com>
2021-12-28 13:26:48 +0330

    main changed

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from Rpi_Face_Recognition.facial import FaceDetector
+import requests
+from rpi_lcd import LCD
+def send_photo_request():
+    url = 'https://35d4e1d5-ca62-4148-99ac-fcbfe6d3758f.mock.pstmn.io/check'
+    files = {'media': open('image.jpg', 'rb')}
+    return requests.post(url, files=files)
 
 
 if __name__ == '__main__':
+    lcd = LCD()
+    led = LED(12, 38, 40)
     face_detector = FaceDetector()
     c = 0
     while True:
         if face_detector.detect_face():
+            lcd.text("Wait for", 1)
+            lcd.text("Green Light!", 2)
             print("Face Detected")
             print(c)
             c+= 1
+            status_code = 0
+            while status_code != 200:
+                response = send_photo_request()
+                try:
+                    data = response.json()
+                    status_code = response.status_code
+                    print(status_code)
+                except requests.exceptions.RequestException:
+                    print(response.text)
             
